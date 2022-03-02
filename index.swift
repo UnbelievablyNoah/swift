b75a73a5e2a274f8975f43587d339ca4e170d38c
n343nc001 <noahcia343@gmail.com>
2022-03-02 16:56:40 -0500

    s

---
+++ index.swift
@@ -0,0 +1,11 @@
+struct fart  {
+    var fart: String
+    var gas: int = 100
+    var smell: [int] = []
+
+    init(_name: String) {
+        self.name = name
+    }
+}
+
+var fart = fart("smelly")
